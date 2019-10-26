3eb1db037f499502dcfd7e486e295c32057882a7
ritikaa17 <40390051+ritikaa17@users.noreply.github.com>
2019-10-26 13:05:04 +0530

    Update Bhavya.java

--- Bhavya.java
+++ Bhavya.java
@@ -4,6 +4,7 @@ public class Bhavya
 {
 	public static void main(String [] args)
 	{
+		System.out.println("Welcome to the application!");
 		System.out.println("This is Bhavya");
 		System.out.println("Profile details should be mentioned here, as follows: \nName:\nAge:\nProfession:\n");
 	}
